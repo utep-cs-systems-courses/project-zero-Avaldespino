e280a5a034bce7918fe0a830b90ca79727db0975
Adrian Valdespino <avaldespino@miners.utep.edu>
2021-09-29 19:15:27 -0600

    Added other fonts

--- draw_chars.c
+++ draw_chars.c
@@ -6,8 +6,42 @@ void print_char_11x16(char c)
   c -= 0x20;
   for (char row = 0; row < 11; row++) {
     unsigned short rowBits = font_11x16[c][row];
+    // printf("Rowbits = %d\n", rowBits);
     for (char col = 0; col < 16; col++) {
       unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+      //printf("ColMask = %d\n", colMask);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+void print_char_5x7(char c)
+{
+  c -= 0x20;
+  
+  for (char row = 0; row < 5; row++) {
+    unsigned char rowBits = font_5x7[c][row];
+    //printf("Rowbits = %d\n", rowBits);
+    for (char col = 0; col < 7; col++) {
+      unsigned short colMask = 1 << (6-col); /* mask to select bit associated with bit */
+      //printf("ColMask = %d\n", colMask);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  
+  for (char row = 0; row < 8; row++) {
+    unsigned short colMask = 1 << (7-row); /* mask to select bit associated with bit */
+   
+    //printf("Rowbits = %d\n", rowBits);
+    for (char col = 0; col < 12; col++) {
+      unsigned char rowBits = font_8x12[c][col];
+      //printf("ColMask = %d\n", colMask);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s,Arrow = a ,Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s,Arrow = a ,Chars = c, Chars 5x7 = 5, Chars 8x12 = 8) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,6 +16,16 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
+    case '8':
+      puts("You selected Chars 8x12");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case '5':
+      puts("You selected Chars 5x7");
+      for (char c = 'a'; c < 'd'; c++)
+	print_char_5x7(c);
+      break;
     case 't':
       puts("You selected triangle:");
       print_triangle(5, 7);
